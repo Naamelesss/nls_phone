afe45bb8c14c46c4da63f4c5ede046814b3542fb
Naamelesss <max.schemmel54@gmail.com>
2022-10-06 14:45:02 +0200

    add routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeScreenComponent } from "./home-screen/home-screen.component";
+
+const routes: Routes = [
+    {path: '', component: HomeScreenComponent},
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {
+
+}
