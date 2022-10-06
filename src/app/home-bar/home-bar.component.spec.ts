4df54af3ef8fdbe3605797d83769e10dc7044bd4
Naamelesss <max.schemmel54@gmail.com>
2022-10-06 15:39:03 +0200

    create home bar component

---
+++ src/app/home-bar/home-bar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeBarComponent } from './home-bar.component';
+
+describe('HomeBarComponent', () => {
+  let component: HomeBarComponent;
+  let fixture: ComponentFixture<HomeBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeBarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
