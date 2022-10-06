d733e1ce8878d4336eb9b800ff38f74bd331e23d
Naamelesss <max.schemmel54@gmail.com>
2022-10-06 15:36:15 +0200

    add phone route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AppPhoneComponent } from "./app-phone/app-phone.component";
 import { HomeScreenComponent } from "./home-screen/home-screen.component";
 
 const routes: Routes = [
     {path: '', component: HomeScreenComponent},
+    {path: 'phone', component: AppPhoneComponent}
 ]
 
 @NgModule({
