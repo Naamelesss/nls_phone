a4f4ccf5c22657ae3eb501c17b0bc4412b1249c4
Naamelesss <max.schemmel54@gmail.com>
2022-10-07 18:27:38 +0200

    create cutom pipe (phone number)

---
+++ src/app/phone-format.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'phoneFormat'
+})
+export class PhoneFormatPipe implements PipeTransform {
+  transform(number: string) {
+    // number = number.charAt(0) != 0 ? "0" + number : "" + number;
+  
+    let newStr = "";
+    let i = 0;
+  
+    for (; i < Math.floor(number.length / 2) - 1; i++) {
+      newStr = newStr + number.substr(i * 2, 2) + " ";
+    }
+  
+    return newStr + number.substr(i * 2);
+  }
+
+}
