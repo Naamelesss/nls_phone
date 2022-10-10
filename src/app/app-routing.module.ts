e8cfbf7d6480c87bf184c442f200b5e259566083
Naamelesss <max.schemmel54@gmail.com>
2022-10-11 00:39:11 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AppContactsAddComponent } from "./app-contacts-add/app-contacts-add.component";
 import { AppContactsComponent } from "./app-contacts/app-contacts.component";
 import { AppPhoneClavierComponent } from "./app-phone-clavier/app-phone-clavier.component";
 import { AppPhoneContactsComponent } from "./app-phone-contacts/app-phone-contacts.component";
@@ -9,7 +10,8 @@ const routes: Routes = [
     {path: '', component: HomeScreenComponent, data: { animationState: 'home' }},
     {path: 'phone/clavier', component: AppPhoneClavierComponent, data: { animationState: 'app' }},
     {path: 'phone/contacts', component: AppPhoneContactsComponent, data: { animationState: 'app' }},
-    {path: 'contacts', component: AppContactsComponent, data: { animationState: 'app' }}
+    {path: 'contacts', component: AppContactsComponent, data: { animationState: 'app' }},
+    {path: 'contacts/add', component: AppContactsAddComponent, data: { animationState: 'app' }}
 ]
 
 @NgModule({
