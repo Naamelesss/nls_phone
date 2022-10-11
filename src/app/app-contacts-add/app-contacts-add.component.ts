10a0855c1646d87297ed65a179bcfae9960d28a4
Naamelesss <max.schemmel54@gmail.com>
2022-10-11 23:14:27 +0200

    add logic

--- src/app/app-contacts-add/app-contacts-add.component.ts
+++ src/app/app-contacts-add/app-contacts-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppsService } from '../services/apps.service';
 
 @Component({
   selector: 'app-app-contacts-add',
@@ -7,20 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppContactsAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppsService) { }
+
+  firstName!: string;
+  lastName!: string;
+  number!: string;
+  note!: string;
 
   firstLetterName!: string;
   secLetterName!: string;
 
   ngOnInit(): void {
+    this.firstName = 'a';
+    this.lastName = 'b';
+    this.number = '5';
+    this.note = 'c';
+
     this.firstLetterName = "";
     this.secLetterName = "";
   }
 
   onChangeName(name: string, type: string): void {
   if (type === 'main') {
-      this.firstLetterName = name.charAt(0);}
-    else if (type === 'sub') {
-      this.secLetterName = name.charAt(0);}
+      this.firstName = name;
+      this.firstLetterName = name.charAt(0);
+  } else if (type === 'sub') {
+      this.lastName = name;
+      this.secLetterName = name.charAt(0);
+    }
+  }
+  onChangeNumber(number: string):void {
+    this.number = number;
+  }
+  onChangeNote(text: string):void {
+    this.note = text;
+  }
+  onValidateNewContact():void {
+    // alert("Name : "+ this.firstName + " | LastName : " + this.lastName + " | Number : " + this.number + " | Note : " + this.note)
+    this.appService.addNewContact(this.firstName, this.lastName, this.number, this.note);
   }
 }
