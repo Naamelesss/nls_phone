3ceb853479dba0122476798fd778b8f857953a43
Naamelesss <max.schemmel54@gmail.com>
2022-10-12 23:11:14 +0200

    add displaying name when number exist in contacts

--- src/app/app-phone-clavier/app-phone-clavier.component.ts
+++ src/app/app-phone-clavier/app-phone-clavier.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppsService } from '../services/apps.service';
 
 @Component({
   selector: 'app-app-phone-clavier',
@@ -7,16 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppPhoneClavierComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppsService) { }
 
   callNumber!: string;
+  contactName!: string;
 
   ngOnInit(): void {
-    this.callNumber = '06457845126';
+    this.callNumber = '0645784512';
+    this.contactName = 'Alex Deckel';
+  }
+
+  searchContact() {
+    this.contactName = '';
+    const search = this.appService.searchContactByNumber(this.callNumber);
+    if (search) {
+      if (search.firstName !== '' && search.lastName !== '') {
+        this.contactName = search.firstName + ' ' + search.lastName;
+      } else {
+        this.contactName = search.firstName + search.lastName;
+      }
+    }
   }
 
   onClickNumber(number_: string) {
     this.callNumber = this.callNumber + number_;
+    this.searchContact();
   }
 
   onCall() {
@@ -25,5 +41,6 @@ export class AppPhoneClavierComponent implements OnInit {
 
   onReturn() {
     this.callNumber = this.callNumber.slice(0, -1);
+    this.searchContact();
   }
 }
