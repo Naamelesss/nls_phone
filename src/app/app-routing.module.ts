3932e400f0cd0f3426e9109162f7dd4df6ba0b8b
Naamelesss <max.schemmel54@gmail.com>
2022-10-17 18:03:27 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AppContactEditComponent } from "./app-contact-edit/app-contact-edit.component";
 import { AppContactsAddComponent } from "./app-contacts-add/app-contacts-add.component";
 import { AppContactsComponent } from "./app-contacts/app-contacts.component";
 import { AppPhoneClavierComponent } from "./app-phone-clavier/app-phone-clavier.component";
 import { AppPhoneContactsComponent } from "./app-phone-contacts/app-phone-contacts.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 import { HomeScreenComponent } from "./home-screen/home-screen.component";
 
 const routes: Routes = [
@@ -11,7 +13,9 @@ const routes: Routes = [
     {path: 'phone/clavier', component: AppPhoneClavierComponent, data: { animationState: 'app' }},
     {path: 'phone/contacts', component: AppPhoneContactsComponent, data: { animationState: 'app' }},
     {path: 'contacts', component: AppContactsComponent, data: { animationState: 'app' }},
-    {path: 'contacts/add', component: AppContactsAddComponent, data: { animationState: 'app' }}
+    {path: 'contacts/add', component: AppContactsAddComponent, data: { animationState: 'app' }},
+    {path: 'contacts/edit/:number', component: AppContactEditComponent},
+    {path: 'gallery', component: GalleryComponent, data: { animationState: 'app' }}
 ]
 
 @NgModule({
