9b46c869b5f5f7d24c081a523b89f99f2b8f577c
Naamelesss <max.schemmel54@gmail.com>
2022-10-22 21:14:10 +0200

    adding routing for recent calls function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AppContactsAddComponent } from "./app-contacts-add/app-contacts-add.com
 import { AppContactsComponent } from "./app-contacts/app-contacts.component";
 import { AppPhoneClavierComponent } from "./app-phone-clavier/app-phone-clavier.component";
 import { AppPhoneContactsComponent } from "./app-phone-contacts/app-phone-contacts.component";
+import { AppPhoneRecentComponent } from "./app-phone-recent/app-phone-recent.component";
 import { GalleryComponent } from "./gallery/gallery.component";
 import { HomeScreenComponent } from "./home-screen/home-screen.component";
 
@@ -13,6 +14,7 @@ const routes: Routes = [
     {path: 'phone/clavier', component: AppPhoneClavierComponent, data: { animationState: 'app' }},
     {path: 'phone/contacts', component: AppPhoneContactsComponent, data: { animationState: 'app' }},
     {path: 'phone/contacts/add', component: AppContactsAddComponent, data: { animationState: 'app' }},
+    {path: 'phone/recent', component: AppPhoneRecentComponent, data: { animationState: 'app' }},
     {path: 'contacts', component: AppContactsComponent, data: { animationState: 'app' }},
     {path: 'contacts/add', component: AppContactsAddComponent, data: { animationState: 'app' }},
     {path: 'contacts/edit/:number', component: AppContactEditComponent},
